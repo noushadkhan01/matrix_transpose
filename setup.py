e90d89b86b1861f37a62b1fe81c8f6400515f32b
Noushad Khan <noushadkhan1994@gmail.com>
2020-05-10 16:55:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="matrix_transpose", # Replace with your own username
-    version="1.1",
+    version="1.3",
     author="Noushad Khan",
     author_email="noushadkhan1994@gmail.com",
     description="matrix transpose",
