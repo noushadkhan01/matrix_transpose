8a8b134f56d68ca08e672690ac7783829f3cf9a3
Noushad Khan <noushadkhan1994@gmail.com>
2020-05-10 15:53:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,22 @@
-from distutils.core import setup
-setup(
-  name = 'matrix_transpose',         # How you named your package folder (MyLib)
-  packages = ['matrix_transpose'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'it transpose a matrix',   # Give a short description about your library
-  author = 'Noushad Khan',                   # Type in your name
-  author_email = 'noushadkhan1994@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/noushadkhan01/matrix_transpose',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/noushadkhan01/matrix_transpose/archive/1.0.tar.gz',    # I explain this later on
-  keywords = ['matrix transpose', 'transpose of a matrix', 'how to transpose a matrix'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-  ],
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="matrix_transpose", # Replace with your own username
+    version="1.0",
+    author="Noushad Khan",
+    author_email="noushadkhan1994@gmail.com",
+    description="matrix transpose",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/noushadkhan01/matrix_transpose",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
