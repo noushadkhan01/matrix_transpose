9c7fdc815480033df851ef0ec3cbcf08e1c24b6f
Noushad Khan <noushadkhan1994@gmail.com>
2020-05-10 15:41:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Noushad Khan',                   # Type in your name
   author_email = 'noushadkhan1994@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/noushadkhan01/matrix_transpose',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/noushadkhan01/matrix_transpose/archive/1.0.tar.gz',    # I explain this later on
   keywords = ['matrix transpose', 'transpose of a matrix', 'how to transpose a matrix'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
